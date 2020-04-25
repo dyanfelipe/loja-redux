cc1905f190a1b57e9bf1cefa94c5d9a40d81512d
dyansilva <dyansilva@outlook.com>
2020-04-25 17:45:26 -0300

    store redux

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+
+import rootReducer from './modules/rootReducer';
+
+const enhance = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+
+const store = createStore(rootReducer, enhance);
+
+export default store;
